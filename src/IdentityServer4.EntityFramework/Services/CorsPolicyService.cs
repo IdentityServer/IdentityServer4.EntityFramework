be517425ee417afb7db43c1c49eccccb1540838b
Scott Brady <scott.brady@rocksolidknowledge.com>
2016-08-30 09:11:58 +0100

    Implemented CorsPolicyService

---
+++ src/IdentityServer4.EntityFramework/Services/CorsPolicyService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.Interfaces;
+using IdentityServer4.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityServer4.EntityFramework.Services
+{
+    public class CorsPolicyService : ICorsPolicyService
+    {
+        private readonly IClientDbContext context;
+
+        public CorsPolicyService(IClientDbContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            this.context = context;
+        }
+
+        public async Task<bool> IsOriginAllowedAsync(string origin)
+        {
+            var origins = await context.Clients.SelectMany(x => x.AllowedCorsOrigins.Select(y => y.Origin)).ToListAsync();
+
+            var distinctOrigins = origins.Where(x => x != null).Distinct();
+            
+            return distinctOrigins.Contains(origin, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ test/IdentityServer4.EntityFramework.IntegrationTests/Services/CorsPolicyServiceTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.EntityFramework.Services;
+using IdentityServer4.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace IdentityServer4.EntityFramework.IntegrationTests.Services
+{
+    public class CorsPolicyServiceTests
+    {
+        public CorsPolicyServiceTests()
+        {
+            var connectionStringBuilder = new SqliteConnectionStringBuilder {DataSource = ":memory:"};
+            var connectionString = connectionStringBuilder.ToString();
+            var connection = new SqliteConnection(connectionString);
+
+            var builder = new DbContextOptionsBuilder<ClientDbContext>();
+            builder.UseSqlite(connection);
+            options = builder.Options;
+
+            using (var context = new ClientDbContext(options))
+            {
+                // Create tables
+                context.Database.OpenConnection();
+                context.Database.EnsureCreated();
+
+                // Add test data
+                context.Clients.Add(
+                    new Client
+                    {
+                        ClientId = "1",
+                        ClientName = "1",
+                        AllowedCorsOrigins = new List<string> {"https://www.identityserver.com"}
+                    }.ToEntity());
+                context.Clients.Add(new Client
+                {
+                    ClientId = "2",
+                    ClientName = "2",
+                    AllowedCorsOrigins = new List<string> {"https://www.identityserver.com", TestCorsOrigin}
+                }.ToEntity());
+
+                context.SaveChanges();
+            }
+        }
+
+        private readonly DbContextOptions<ClientDbContext> options;
+
+        private const string TestCorsOrigin = "https://identityserver.io";
+
+        [Fact]
+        public void IsOriginAllowedAsync_WhenOriginIsAllowed_ExpectTrue()
+        {
+            bool result;
+            using (var context = new ClientDbContext(options))
+            {
+                var service = new CorsPolicyService(context);
+                result = service.IsOriginAllowedAsync(TestCorsOrigin).Result;
+            }
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsOriginAllowedAsync_WhenOriginIsNotAllowed_ExpectFalse()
+        {
+            bool result;
+            using (var context = new ClientDbContext(options))
+            {
+                var service = new CorsPolicyService(context);
+                result = service.IsOriginAllowedAsync("InvalidOrigin").Result;
+            }
+
+            Assert.False(result);
+        }
+    }
+}
