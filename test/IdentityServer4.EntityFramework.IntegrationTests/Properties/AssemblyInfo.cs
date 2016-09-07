ec9da167a431596e921cbe11b3eee0f155a4b854
Brock Allen <brockallen@gmail.com>
2016-09-07 09:04:56 -0400

    disable parallel tests

--- test/IdentityServer4.EntityFramework.IntegrationTests/Properties/AssemblyInfo.cs
+++ test/IdentityServer4.EntityFramework.IntegrationTests/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Xunit;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -17,3 +18,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("73cce3c6-5431-4b76-acbe-ef4ee76d6b81")]
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
