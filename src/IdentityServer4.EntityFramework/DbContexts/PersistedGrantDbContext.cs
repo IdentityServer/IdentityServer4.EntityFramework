4d9465e7719334dc4e8f8b027de2e4a1e91382a0
David-John Miller <davidjohn.miller@is.co.za>
2016-09-07 11:07:07 +0200

    Break out ModelBuilder on the DbContexts and expose them as extensions. This makes it much easier for integrators to be able to set up migrations on a single context if they desire

--- src/IdentityServer4.EntityFramework/DbContexts/PersistedGrantDbContext.cs
+++ src/IdentityServer4.EntityFramework/DbContexts/PersistedGrantDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using IdentityServer4.EntityFramework.Entities;
+using IdentityServer4.EntityFramework.Extensions;
 using IdentityServer4.EntityFramework.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,17 +19,7 @@ namespace IdentityServer4.EntityFramework.DbContexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<PersistedGrant>(grant =>
-            {
-                grant.ToTable(EfConstants.TableNames.PersistedGrant);
-                grant.HasKey(x => new {x.Key, x.Type});
-                grant.Property(x => x.SubjectId).IsRequired();
-                grant.Property(x => x.ClientId).HasMaxLength(200).IsRequired();
-                grant.Property(x => x.CreationTime).IsRequired();
-                grant.Property(x => x.Expiration).IsRequired();
-                grant.Property(x => x.Data).IsRequired();
-
-            });
+            modelBuilder.ConfigurePersistedGrantContext();
 
             base.OnModelCreating(modelBuilder);
         }
