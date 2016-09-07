7fb91ae2572dd54c5f084303e987216c9814a379
Brock Allen <brockallen@gmail.com>
2016-09-07 17:28:52 -0400

    formatting

--- test/IdentityServer4.EntityFramework.UnitTests/Mappers/ClientMappersTests.cs
+++ test/IdentityServer4.EntityFramework.UnitTests/Mappers/ClientMappersTests.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+
 using System.Collections.Generic;
 using IdentityServer4.EntityFramework.Entities;
 using IdentityServer4.EntityFramework.Mappers;
