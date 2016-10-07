34e27200e191798e2e7fcdcbd8be0b0e8c3b887b
Brock Allen <brockallen@gmail.com>
2016-10-07 11:15:56 -0400

    update config

--- src/Host/Configuration/Clients.cs
+++ src/Host/Configuration/Clients.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -19,18 +20,32 @@ namespace Host.Configuration
                 new Client
                 {
                     ClientId = "client",
-                    ClientName = "client",
-                    ClientSecrets = new List<Secret>
+                    ClientSecrets =
                     {
                         new Secret("secret".Sha256())
                     },
 
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "api1", "api2" },
+                },
 
-                    AllowedScopes = new List<string>
+                ///////////////////////////////////////////
+                // Console Client Credentials Flow with client JWT assertion
+                //////////////////////////////////////////
+                new Client
+                {
+                    ClientId = "client.jwt",
+                    ClientSecrets =
                     {
-                        "api1", "api2"
-                    }
+                        new Secret
+                        {
+                            Type = IdentityServerConstants.SecretTypes.X509CertificateBase64,
+                            Value = "MIIDATCCAe2gAwIBAgIQoHUYAquk9rBJcq8W+F0FAzAJBgUrDgMCHQUAMBIxEDAOBgNVBAMTB0RldlJvb3QwHhcNMTAwMTIwMjMwMDAwWhcNMjAwMTIwMjMwMDAwWjARMQ8wDQYDVQQDEwZDbGllbnQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDSaY4x1eXqjHF1iXQcF3pbFrIbmNw19w/IdOQxbavmuPbhY7jX0IORu/GQiHjmhqWt8F4G7KGLhXLC1j7rXdDmxXRyVJBZBTEaSYukuX7zGeUXscdpgODLQVay/0hUGz54aDZPAhtBHaYbog+yH10sCXgV1Mxtzx3dGelA6pPwiAmXwFxjJ1HGsS/hdbt+vgXhdlzud3ZSfyI/TJAnFeKxsmbJUyqMfoBl1zFKG4MOvgHhBjekp+r8gYNGknMYu9JDFr1ue0wylaw9UwG8ZXAkYmYbn2wN/CpJl3gJgX42/9g87uLvtVAmz5L+rZQTlS1ibv54ScR2lcRpGQiQav/LAgMBAAGjXDBaMBMGA1UdJQQMMAoGCCsGAQUFBwMCMEMGA1UdAQQ8MDqAENIWANpX5DZ3bX3WvoDfy0GhFDASMRAwDgYDVQQDEwdEZXZSb290ghAsWTt7E82DjU1E1p427Qj2MAkGBSsOAwIdBQADggEBADLje0qbqGVPaZHINLn+WSM2czZk0b5NG80btp7arjgDYoWBIe2TSOkkApTRhLPfmZTsaiI3Ro/64q+Dk3z3Kt7w+grHqu5nYhsn7xQFAQUf3y2KcJnRdIEk0jrLM4vgIzYdXsoC6YO+9QnlkNqcN36Y8IpSVSTda6gRKvGXiAhu42e2Qey/WNMFOL+YzMXGt/nDHL/qRKsuXBOarIb++43DV3YnxGTx22llhOnPpuZ9/gnNY7KLjODaiEciKhaKqt/b57mTEz4jTF4kIg6BP03MUfDXeVlM1Qf1jB43G2QQ19n5lUiqTpmQkcfLfyci2uBZ8BkOhXr3Vk9HIk/xBXQ="
+                        }
+                    },
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "api1", "api2" }
                 },
 
                 ///////////////////////////////////////////
@@ -39,18 +54,13 @@ namespace Host.Configuration
                 new Client
                 {
                     ClientId = "client.custom",
-                    ClientName = "client.custom",
-                    ClientSecrets = new List<Secret>
+                    ClientSecrets = 
                     {
                         new Secret("secret".Sha256())
                     },
 
                     AllowedGrantTypes = GrantTypes.List("custom"),
-
-                    AllowedScopes = new List<string>
-                    {
-                        "api1", "api2"
-                    }
+                    AllowedScopes = { "api1", "api2" }
                 },
 
                 ///////////////////////////////////////////
@@ -59,15 +69,14 @@ namespace Host.Configuration
                 new Client
                 {
                     ClientId = "roclient",
-                    ClientName = "roclient",
-                    ClientSecrets = new List<Secret>
+                    ClientSecrets = 
                     {
                         new Secret("secret".Sha256())
                     },
 
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
 
-                    AllowedScopes = new List<string>
+                    AllowedScopes = 
                     {
                         StandardScopes.OpenId.Name,
                         StandardScopes.Email.Name,
@@ -83,12 +92,11 @@ namespace Host.Configuration
                 new Client
                 {
                     ClientId = "roclient.public",
-                    ClientName = "roclient.public",
                     RequireClientSecret = false,
 
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
 
-                    AllowedScopes = new List<string>
+                    AllowedScopes = 
                     {
                         StandardScopes.OpenId.Name,
                         StandardScopes.Email.Name,
@@ -110,12 +118,9 @@ namespace Host.Configuration
                     AllowedGrantTypes = GrantTypes.Hybrid,
                     RequirePkce = true,
 
-                    RedirectUris = new List<string>
-                    {
-                        "http://127.0.0.1:7890/"
-                    },
+                    RedirectUris = { "http://127.0.0.1:7890/" },
 
-                    AllowedScopes = new List<string>
+                    AllowedScopes =
                     {
                         StandardScopes.OpenId.Name,
                         StandardScopes.Profile.Name,
@@ -133,18 +138,13 @@ namespace Host.Configuration
                 new Client
                 {
                     ClientId = "roclient.reference",
-                    ClientName = "roclient.reference",
-                    ClientSecrets = new List<Secret>
+                    ClientSecrets = 
                     {
                         new Secret("secret".Sha256())
                     },
 
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-
-                    AllowedScopes = new List<string>
-                    {
-                        "api1", "api2"
-                    },
+                    AllowedScopes = { "api1", "api2" },
 
                     AccessTokenType = AccessTokenType.Reference
                 },
@@ -160,17 +160,12 @@ namespace Host.Configuration
 
                     AllowedGrantTypes = GrantTypes.Implicit,
                     AllowAccessTokensViaBrowser = true,
-                    RedirectUris = new List<string>
-                    {
-                        "http://localhost:44077/signin-oidc"
-                    },
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        "http://localhost:44077/"
-                    },
-                    LogoutUri = "http://localhost:44077/signout-oidc",
 
-                    AllowedScopes = new List<string>
+                    RedirectUris =  { "http://localhost:44077/signin-oidc" },
+                    LogoutUri = "http://localhost:44077/signout-oidc",
+                    PostLogoutRedirectUris = { "http://localhost:44077/" },
+
+                    AllowedScopes =
                     {
                         StandardScopes.OpenId.Name,
                         StandardScopes.Profile.Name,
@@ -182,31 +177,45 @@ namespace Host.Configuration
                 },
 
                 ///////////////////////////////////////////
+                // MVC Manual Implicit Flow Sample
+                //////////////////////////////////////////
+                new Client
+                {
+                    ClientId = "mvc.manual",
+                    ClientName = "MVC Manual",
+                    ClientUri = "http://identityserver.io",
+
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    RedirectUris = { "http://localhost:44077/home/callback" },
+                    LogoutUri = "http://localhost:44077/signout-oidc",
+                    PostLogoutRedirectUris = { "http://localhost:44077/" },
+
+                    AllowedScopes = { StandardScopes.OpenId.Name },
+                },
+
+                ///////////////////////////////////////////
                 // MVC Hybrid Flow Samples
                 //////////////////////////////////////////
                 new Client
                 {
                     ClientId = "mvc.hybrid",
                     ClientName = "MVC Hybrid",
-                    ClientSecrets = new List<Secret>
+                    ClientUri = "http://identityserver.io",
+
+                    ClientSecrets = 
                     {
                         new Secret("secret".Sha256())
                     },
-                    ClientUri = "http://identityserver.io",
-
+                    
                     AllowedGrantTypes = GrantTypes.Hybrid,
                     AllowAccessTokensViaBrowser = false,
-                    RedirectUris = new List<string>
-                    {
-                        "http://localhost:21402/signin-oidc"
-                    },
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        "http://localhost:21402/"
-                    },
-                    LogoutUri = "http://localhost:21402/signout-oidc",
 
-                    AllowedScopes = new List<string>
+                    RedirectUris = { "http://localhost:21402/signin-oidc" },
+                    LogoutUri = "http://localhost:21402/signout-oidc",
+                    PostLogoutRedirectUris = { "http://localhost:21402/" },
+
+                    AllowedScopes = 
                     {
                         StandardScopes.OpenId.Name,
                         StandardScopes.Profile.Name,
@@ -229,15 +238,9 @@ namespace Host.Configuration
 
                     AllowedGrantTypes = GrantTypes.Implicit,
                     AllowAccessTokensViaBrowser = true,
-                    RedirectUris = new List<string>
-                    {
-                        "http://localhost:28895/index.html"
-                    },
 
-                    AllowedScopes = new List<string>
-                    {
-                        "api1", "api2"
-                    },
+                    RedirectUris = { "http://localhost:28895/index.html" },
+                    AllowedScopes = { "api1", "api2" },
                 },
                 
                 ///////////////////////////////////////////
@@ -251,24 +254,19 @@ namespace Host.Configuration
 
                     AllowedGrantTypes = GrantTypes.Implicit,
                     AllowAccessTokensViaBrowser = true,
+                    RequireClientSecret = false,
                     AccessTokenType = AccessTokenType.Reference,
 
-                    RedirectUris = new List<string>
+                    RedirectUris = 
                     {
                         "http://localhost:7017/index.html",
                         "http://localhost:7017/silent_renew.html",
                     },
-                    PostLogoutRedirectUris = new List<string>
-                    {
-                        "http://localhost:7017/index.html",
-                    },
 
-                    AllowedCorsOrigins = new List<string>
-                    {
-                        "http://localhost:7017"
-                    },
+                    PostLogoutRedirectUris = { "http://localhost:7017/index.html" },
+                    AllowedCorsOrigins = { "http://localhost:7017" },
 
-                    AllowedScopes = new List<string>
+                    AllowedScopes =
                     {
                         StandardScopes.OpenId.Name,
                         StandardScopes.Profile.Name,
