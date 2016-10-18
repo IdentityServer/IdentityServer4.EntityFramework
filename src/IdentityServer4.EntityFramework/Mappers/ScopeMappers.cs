a25e6f313f0bfddd5df8fe8c59cc932517e01c3f
Mien Wu <mien@dreamxo.net>
2016-10-19 01:02:48 +0800

    just tidy the code.

--- src/IdentityServer4.EntityFramework/Mappers/ScopeMappers.cs
+++ src/IdentityServer4.EntityFramework/Mappers/ScopeMappers.cs
@@ -5,9 +5,6 @@
 using System.Linq;
 using AutoMapper;
 using IdentityServer4.EntityFramework.Entities;
-using IdentityServer4.Models;
-using Scope = IdentityServer4.Models.Scope;
-using ScopeClaim = IdentityServer4.EntityFramework.Entities.ScopeClaim;
 
 namespace IdentityServer4.EntityFramework.Mappers
 {
@@ -17,35 +14,33 @@ namespace IdentityServer4.EntityFramework.Mappers
         {
             Mapper = new MapperConfiguration(config =>
             {
-                config.CreateMap<Entities.Scope, Scope>(MemberList.Destination)
+                // entity to model
+                config.CreateMap<Scope, Models.Scope>(MemberList.Destination)
                     .ForMember(x => x.Claims, opt => opt.MapFrom(src => src.Claims.Select(x => x)))
                     .ForMember(x => x.ScopeSecrets, opts => opts.MapFrom(src => src.ScopeSecrets.Select(x => x)));
                 config.CreateMap<ScopeClaim, Models.ScopeClaim>(MemberList.Destination);
-                config.CreateMap<ScopeSecret, Secret>(MemberList.Destination)
+                config.CreateMap<ScopeSecret,Models.Secret>(MemberList.Destination)
                     .ForMember(dest => dest.Type, opt => opt.Condition(srs => srs != null));
 
-                config.CreateMap<Scope, Entities.Scope>(MemberList.Source)
+                // model to entity
+                config.CreateMap<Models.Scope, Scope>(MemberList.Source)
                     .ForMember(x => x.Claims, opts => opts.MapFrom(src => src.Claims.Select(x => x)))
                     .ForMember(x => x.ScopeSecrets, opts => opts.MapFrom(src => src.ScopeSecrets.Select(x => x)));
                 config.CreateMap<Models.ScopeClaim, ScopeClaim>(MemberList.Source);
-                config.CreateMap<Secret, ScopeSecret>(MemberList.Source);
+                config.CreateMap<Models.Secret, ScopeSecret>(MemberList.Source);
             }).CreateMapper();
         }
 
         internal static IMapper Mapper { get; }
 
-        public static Scope ToModel(this Entities.Scope scope)
+        public static Models.Scope ToModel(this Scope scope)
         {
-            if (scope == null) return null;
-
-            return Mapper.Map<Entities.Scope, Scope>(scope);
+            return scope == null ? null : Mapper.Map<Models.Scope>(scope);
         }
 
-        public static Entities.Scope ToEntity(this Scope scope)
+        public static Scope ToEntity(this Models.Scope scope)
         {
-            if (scope == null) return null;
-
-            return Mapper.Map<Scope, Entities.Scope>(scope);
+            return scope == null ? null : Mapper.Map<Scope>(scope);
         }
     }
 }
