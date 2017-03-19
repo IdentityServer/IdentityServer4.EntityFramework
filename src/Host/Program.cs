d7eb5bae1aaecd3a76e272c3418621132d935d18
Brock Allen <brockallen@gmail.com>
2017-03-19 12:53:40 -0400

    update image for appveyor

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -16,7 +16,7 @@ namespace Host
 
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://localhost:1941")
+                .UseUrls("http://localhost:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
