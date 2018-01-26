9f70a98585fa87d1b67ed1862ee8871f3600e904
Scott Brady <scottbrady91@outlook.com>
2018-01-26 10:20:15 +0000

    Reduced database names for MySQL & PostgreSQL

--- test/IdentityServer4.EntityFramework.IntegrationTests/DatabaseProviderBuilder.cs
+++ test/IdentityServer4.EntityFramework.IntegrationTests/DatabaseProviderBuilder.cs
@@ -43,14 +43,14 @@ namespace IdentityServer4.EntityFramework.IntegrationTests
         public static DbContextOptions<T> BuildAppVeyorMySql<T>(string name) where T : DbContext
         {
             var builder = new DbContextOptionsBuilder<T>();
-            builder.UseMySql($@"server=localhost;database=Test.IdentityServer4.EntityFramework-2.0.0.{name};userid=root;pwd=Password12!;port=3306;persistsecurityinfo=True;");
+            builder.UseMySql($@"server=localhost;database=Test.IdentityServer4-2.0.0.{name};userid=root;pwd=Password12!;port=3306;persistsecurityinfo=True;");
             return builder.Options;
         }
 
         public static DbContextOptions<T> BuildAppVeyorPostgreSql<T>(string name) where T : DbContext
         {
             var builder = new DbContextOptionsBuilder<T>();
-            builder.UseNpgsql($@"Host=localhost;Database=Test.IdentityServer4.EntityFramework-2.0.0.{name};Username=postgres;Password=Password12!;Port=5432;");
+            builder.UseNpgsql($@"Host=localhost;Database=Test.IdentityServer4-2.0.0.{name};Username=postgres;Password=Password12!;Port=5432;");
             return builder.Options;
         }
     }
